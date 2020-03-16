feacb66565c52082b5bbe42578c8b413f5cc145c
dueduardoful <dueduardoful@gmail.com>
2020-03-16 15:42:39 -0300

    Instanciando EntityManager e EMF

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,19 +1,24 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Eduardo", "dueduardoful@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Fulano", "fulano@gmail.com");
-		Pessoa p3 = new Pessoa(3, "cilcrano", "cilcrano@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Eduardo", "dueduardoful@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Fulano", "fulano@gmail.com");
+		Pessoa p3 = new Pessoa(null, "cilcrano", "cilcrano@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
-		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
 		
 	}
 
